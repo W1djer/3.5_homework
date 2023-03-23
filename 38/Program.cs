7bad2238e95b87a265c3fed98c3dea7e1e15e24a
Evgenii <w1djer@mail.ru>
2023-03-23 12:18:20 +0300

    выполнена 38

---
+++ 38/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(0, 101);
+}
+void Release(int[] array)
+{
+    int min = array[0];
+    int max = array[1];
+    if (min > max)
+    {
+        min = array[1];
+        max = array[0];
+    }
+    int dif = 0;
+    for(int i = 2; i < array.Length; i++)
+        if (array[i] > max)
+            max = array[i];
+        else if (array[i] < min)
+            min = array[i];
+    Console.WriteLine($"Минимальное: {min}");
+    Console.WriteLine($"Максимальное: {max}");
+    Console.WriteLine($"Разница: {dif = max - min}");
+}
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Массив [{string.Join(", ", array)}]");
+Release(array);
