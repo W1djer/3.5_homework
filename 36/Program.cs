3e074372b2ff42e6cbc88de0bba385287b2f7dc7
Evgenii <w1djer@mail.ru>
2023-03-23 12:08:05 +0300

    выполнена 36

---
+++ 36/Program.cs
@@ -0,0 +1,16 @@
+﻿void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101);
+}
+void Release(int[] array)
+{
+    int sum = 0;
+    for(int i = 1; i < array.Length; i+=2)
+        sum += array[i];
+    Console.WriteLine($"Сумма нечетных позиций: {sum}");
+}
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Массив [{string.Join(", ", array)}]");
+Release(array);
