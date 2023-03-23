17485dd13cc24c96971d46c0307f200db39b9440
Evgenii <w1djer@mail.ru>
2023-03-23 11:56:47 +0300

    выполнена 34

--- 34/Program.cs
+++ 34/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+void Release(int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+        if (array[i]%2 == 0)
+            count++;
+    Console.WriteLine($"Количество четных чисел: {count}");
+}
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Массив [{string.Join(", ", array)}]");
+Release(array);
